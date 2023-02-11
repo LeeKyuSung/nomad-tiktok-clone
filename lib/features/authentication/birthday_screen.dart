580edec3bfd3e6db9bc4ce65b0eac065c8e5881d
LeeKyuSung <ks5050577@gmail.com>
2023-02-12 02:03:14 +0900

    Password Screen

---
+++ lib/features/authentication/birthday_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class BirthdayScreen extends StatelessWidget {
+  const BirthdayScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
