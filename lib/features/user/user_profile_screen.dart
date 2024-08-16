824e68f023745b648b0c053511a1ff9b0dce9cb8
LeeKyuSung <ks5050577@gmail.com>
2024-08-17 00:10:58 +0900

    #12.2 SliverAppBar

--- lib/features/user/user_profile_screen.dart
+++ lib/features/user/user_profile_screen.dart
@@ -16,6 +16,7 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
           floating: true,
           stretch: true,
           pinned: true,
+          snap: true,
           backgroundColor: Colors.teal,
           elevation: 1,
           collapsedHeight: 80,
@@ -24,6 +25,7 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
             stretchModes: const [
               StretchMode.blurBackground,
               StretchMode.zoomBackground,
+              StretchMode.fadeTitle
             ],
             background: Image.asset(
               "assets/images/placeholder.jpg",
@@ -34,6 +36,19 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
                 const EdgeInsets.symmetric(horizontal: 0, vertical: 10),
           ),
         ),
+        SliverFixedExtentList(
+          delegate: SliverChildBuilderDelegate(
+            childCount: 100,
+            (context, index) => Container(
+              color: Colors.amber[100 * (index % 9)],
+              child: Align(
+                alignment: Alignment.center,
+                child: Text("Item $index"),
+              ),
+            ),
+          ),
+          itemExtent: 100,
+        )
       ],
     );
   }
