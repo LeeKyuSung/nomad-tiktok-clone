f367b76e59af9a725c922f6c0c9db49aaec505ef
LeeKyuSung <ks5050577@gmail.com>
2024-08-25 20:18:09 +0900

    Update video_preview_screen.dart

--- lib/features/videos/video_preview_screen.dart
+++ lib/features/videos/video_preview_screen.dart
@@ -2,6 +2,8 @@ import 'dart:io';
 
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:gallery_saver/gallery_saver.dart';
 import 'package:video_player/video_player.dart';
 
 class VideoPreviewScreen extends StatefulWidget {
@@ -17,6 +19,7 @@ class VideoPreviewScreen extends StatefulWidget {
 
 class _VideoPreviewScreenState extends State<VideoPreviewScreen> {
   late final VideoPlayerController _videoPlayerController;
+  bool _savedVideo = false;
 
   Future<void> _initVideo() async {
     _videoPlayerController =
@@ -33,12 +36,50 @@ class _VideoPreviewScreenState extends State<VideoPreviewScreen> {
     _initVideo();
   }
 
+  Future convertTempFileToVideo(String tempFilePath) async {
+    final tempFile = File(tempFilePath);
+    final fileContent = await tempFile.readAsBytes();
+
+    final newFilePath = tempFilePath.replaceFirst('.temp', '.mp4');
+
+    final newFile = File(newFilePath);
+    await newFile.writeAsBytes(fileContent);
+    return newFilePath;
+  }
+
+  Future<void> _saveToGallery() async {
+    if (_savedVideo) return;
+
+    String newFilePath;
+    if (widget.video.path.endsWith(".temp")) {
+      newFilePath = await convertTempFileToVideo(widget.video.path);
+    } else {
+      newFilePath = widget.video.path;
+    }
+    final result = await GallerySaver.saveVideo(
+      newFilePath,
+      albumName: "Tictok_Clone",
+    );
+    if (result == true) {
+      _savedVideo = true;
+      setState(() {});
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.black,
         title: const Text("Preview video"),
+        actions: [
+          IconButton(
+            onPressed: _saveToGallery,
+            icon: FaIcon(_savedVideo
+                ? FontAwesomeIcons.check
+                : FontAwesomeIcons.download),
+          )
+        ],
       ),
       body: _videoPlayerController.value.isInitialized
           ? AspectRatio(
