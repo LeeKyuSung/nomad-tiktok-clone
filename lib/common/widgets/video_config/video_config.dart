f0d5a7c6d954dcd7ec49582a531a84883240fc66
LeeKyuSung <ks5050577@gmail.com>
2024-08-27 00:25:00 +0900

    #20.7 InheritedWidget part Two

--- lib/common/widgets/video_config/video_config.dart
+++ lib/common/widgets/video_config/video_config.dart
@@ -1,16 +1,52 @@
 import 'package:flutter/widgets.dart';
 
-class VideoConfig extends InheritedWidget {
-  const VideoConfig({super.key, required super.child});
+class VideoConfigData extends InheritedWidget {
+  final bool autoMute;
+  final void Function() toggleMuted;
+  const VideoConfigData({
+    super.key,
+    required this.toggleMuted,
+    required this.autoMute,
+    required super.child,
+  });
 
-  final bool autoMute = true;
-
-  static VideoConfig of(BuildContext context) {
-    return context.dependOnInheritedWidgetOfExactType<VideoConfig>()!;
+  static VideoConfigData of(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<VideoConfigData>()!;
   }
 
   @override
   bool updateShouldNotify(covariant InheritedWidget oldWidget) {
     return true;
+  }
+}
+
+class VideoConfig extends StatefulWidget {
+  final Widget child;
+
+  const VideoConfig({
+    super.key,
+    required this.child,
+  });
+
+  @override
+  State<VideoConfig> createState() => _VideoConfigState();
+}
+
+class _VideoConfigState extends State<VideoConfig> {
+  bool autoMute = false;
+
+  void toggleMuted() {
+    setState(() {
+      autoMute = !autoMute;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return VideoConfigData(
+      toggleMuted: toggleMuted,
+      autoMute: autoMute,
+      child: widget.child,
+    );
   }
 }
