84ad407008f687d4f061e8a8107b8978f83e54ba
LeeKyuSung <ks5050577@gmail.com>
2024-09-01 23:25:26 +0900

    #22.4 AsyncNotifierProvider

--- lib/features/videos/views/video_preview_screen.dart
+++ lib/features/videos/views/video_preview_screen.dart
@@ -2,11 +2,13 @@ import 'dart:io';
 
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:gallery_saver/gallery_saver.dart';
+import 'package:tiktok_clone/features/videos/view_models/timeline_view_model.dart';
 import 'package:video_player/video_player.dart';
 
-class VideoPreviewScreen extends StatefulWidget {
+class VideoPreviewScreen extends ConsumerStatefulWidget {
   final XFile video;
   final bool isPicked;
   const VideoPreviewScreen({
@@ -16,10 +18,10 @@ class VideoPreviewScreen extends StatefulWidget {
   });
 
   @override
-  State<VideoPreviewScreen> createState() => _VideoPreviewScreenState();
+  VideoPreviewScreenState createState() => VideoPreviewScreenState();
 }
 
-class _VideoPreviewScreenState extends State<VideoPreviewScreen> {
+class VideoPreviewScreenState extends ConsumerState<VideoPreviewScreen> {
   late final VideoPlayerController _videoPlayerController;
   bool _savedVideo = false;
 
@@ -42,6 +44,10 @@ class _VideoPreviewScreenState extends State<VideoPreviewScreen> {
   void dispose() {
     _videoPlayerController.dispose();
     super.dispose();
+  }
+
+  void _onUploadPressed() async {
+    ref.read(timelineProvider.notifier).uploadVideo();
   }
 
   Future convertTempFileToVideo(String tempFilePath) async {
@@ -78,7 +84,7 @@ class _VideoPreviewScreenState extends State<VideoPreviewScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.black,
+        // backgroundColor: Colors.black,
         title: const Text("Preview video"),
         actions: [
           if (!widget.isPicked)
@@ -89,7 +95,15 @@ class _VideoPreviewScreenState extends State<VideoPreviewScreen> {
                     ? FontAwesomeIcons.check
                     : FontAwesomeIcons.download,
               ),
-            )
+            ),
+          IconButton(
+            onPressed: ref.watch(timelineProvider).isLoading
+                ? () {}
+                : _onUploadPressed,
+            icon: ref.watch(timelineProvider).isLoading
+                ? const CircularProgressIndicator()
+                : const FaIcon(FontAwesomeIcons.cloudArrowUp),
+          )
         ],
       ),
       body: _videoPlayerController.value.isInitialized
