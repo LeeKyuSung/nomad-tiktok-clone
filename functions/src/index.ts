ef8c65507f03360004794d3a63bc2b5ae970fed1
LeeKyuSung <ks5050577@gmail.com>
2024-09-21 17:00:55 +0900

    #26.3 Cloud Functions

---
+++ functions/src/index.ts
@@ -0,0 +1,29 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * import {onCall} from "firebase-functions/v2/https";
+ * import {onDocumentWritten} from "firebase-functions/v2/firestore";
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+// import { onRequest } from "firebase-functions/v2/https";
+// import * as logger from "firebase-functions/logger";
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+
+// Start writing functions
+// https://firebase.google.com/docs/functions/typescript
+
+// export const helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+
+admin.initializeApp();
+
+export const onVideoCreated = functions.firestore
+  .document("videos/{videoId}")
+  .onCreate(async (snapshot, context) => {
+    await snapshot.ref.update({ hello: "from functions" });
+  });
