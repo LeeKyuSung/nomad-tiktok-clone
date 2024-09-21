64617cf35da89543f82a835e8a9425470cd40ca7
LeeKyuSung <ks5050577@gmail.com>
2024-09-21 16:31:21 +0900

    #26.1 VideosRepository, #26.2 uploadVideoProvider

--- lib/features/videos/models/video_model.dart
+++ lib/features/videos/models/video_model.dart
@@ -1,4 +1,36 @@
 class VideoModel {
-  String title;
-  VideoModel({required this.title});
+  final String title;
+  final String description;
+  final String fileUrl;
+  final String thumbnailUrl;
+  final String creatorUid;
+  final String creator;
+  final int likes;
+  final int comments;
+  final int createdAt;
+  VideoModel({
+    required this.title,
+    required this.description,
+    required this.fileUrl,
+    required this.thumbnailUrl,
+    required this.creatorUid,
+    required this.creator,
+    required this.likes,
+    required this.comments,
+    required this.createdAt,
+  });
+
+  Map<String, dynamic> toJson() {
+    return {
+      "title": title,
+      "description": description,
+      "fileUrl": fileUrl,
+      "thumbnailUrl": thumbnailUrl,
+      "creatorUid": creatorUid,
+      "likes": likes,
+      "comments": comments,
+      "createdAt": createdAt,
+      "creator": creator,
+    };
+  }
 }
